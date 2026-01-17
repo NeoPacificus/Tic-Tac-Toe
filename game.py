5ca52a7ebd950b204d06dfbbfa8606dbef222bce
NeoPacificus <pacificus.softwares@gmail.com>
2026-01-17 15:40:31 +0530

    Initial set of instructions

--- game.py
+++ game.py
@@ -1,1 +1,6 @@
+print("Welcome to Tic-Tac-Toe game!")
+game_type = input("Type "a" for a single player game\nType "b" for 2 players game")
 
+Player-1 = input("Enter Player-1 name: ")
+if game_type = "b":
+    Player-2 = input("Enter Player-2 name: ")
